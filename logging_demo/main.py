96f8ad5a2424ceb0fdb5230182dbcaab8ded00b8
Brian D'Ostilio <brian@mountaindigital.io>
2023-12-14 19:41:47 -0500

    initial commit

---
+++ logging_demo/main.py
@@ -0,0 +1,17 @@
+import datetime
+import logging
+import time
+
+import google.cloud.logging as gcp_logging
+
+def main():
+    client = gcp_logging.Client()
+    client.setup_logging()
+    
+    while True:
+        now = datetime.datetime.now()
+        logging.info(f'VM is running at {now}')
+        time.sleep(60 * 10)
+
+if __name__ == '__main__':
+    main()
